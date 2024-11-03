73a6bc898e09f5e00bcddd2a4f599e303d649de6
Mr.JF <mahdi.jahanfar1384@gmail.com>
2024-11-03 17:17:27 +0330

    celery conf file added

--- celery_file.py
+++ celery_file.py
@@ -1,3 +1,5 @@
 import celery
-
+from celery.schedules import crontab
 app = celery.Celery('weather_project', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
+
+app.config_from_object('celery_conf')
